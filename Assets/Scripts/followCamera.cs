11da0b181ef2a27be190d26660f2e911947106c7
ZacharyAydelotte <36612711+ZacharyAydelotte@users.noreply.github.com>
2019-11-04 13:11:02 -0500

    New cameraFollow Script
    cameraFollow script allows for an offset camera following the character.

---
+++ Assets/Scripts/followCamera.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class followCamera : MonoBehaviour
+{ 
+public float interpVelocity;
+public float minDistance;
+public float followDistance;
+public GameObject target;
+public Vector3 offset;
+    public Vector3 targetOffset;
+Vector3 targetPos;
+// Use this for initialization
+void Start()
+{
+    targetPos = transform.position;
+}
+
+// Update is called once per frame
+void Update()
+{
+        if (target.transform.localScale.x > 0)
+        {
+            targetOffset = new Vector3(5, 0, -80);
+        }
+        else
+        {
+            targetOffset = new Vector3(-5, 0, -80);
+        }
+
+        offset = Vector3.Lerp(offset, targetOffset, Time.deltaTime*4);
+
+        transform.position = target.transform.position + offset;
+
+
+    }
+}
